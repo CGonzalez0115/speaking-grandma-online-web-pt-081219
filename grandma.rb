a2f4be8e485e30cacb165d2e88d448171642b271
Christina Gonzalez <cmgamway@gmail.com>
2019-08-17 16:54:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ if speak_to_grandma == "HI NANA, HOW ARE YOU?"
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 
-if ("responds with HUH?! SPEAK UP, SONNY! for a second time")
+if ("responds with HUH?! SPEAK UP, SONNY! for a second time") do
   speak_to_grandma == "Hi"
   puts "HUH?! SPEAK UP, SONNY!"
 end
